7b8d64b7f88a30a7e2b586d17d9bfe02227563ac
Cono <everydaycono@gmail.com>
2023-07-31 11:34:32 +0900

    Add Prisma for mysql database

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+import { PrismaClient } from '@prisma/client';
 import express from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+// Create Prisma client instance.
+const prisma = new PrismaClient();
 
 app.get('/', (req, res) => {
   res.send('wanted-pre-onboarding-backend!');
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT} now!`));
+
+// Signal Interrupt 발생 Prisma 클라이언트 연결을 해제합니다.
+process.on('SIGINT', async () => {
+  await prisma.$disconnect();
+  process.exit();
+});
